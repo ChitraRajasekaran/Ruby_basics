4eb28663f54ccdfe232cfa9a6093ebf807f8eda7
Chitra Rajasekaran <chitrarajasekaran@chitras-air.lan>
2022-11-20 08:51:39 -0500

    The basics - part 1

---
+++ Ruby_basics.rb
@@ -0,0 +1,19 @@
+puts 'Hello,world'  #String literal
+puts 200 #integer literal
+puts 2.7 #float literal
+puts true #boolean literal
+puts ({ 'a' => 1, 'b' => 2 })  #hash literal
+puts [1,2,3] #array literal
+puts :txt #symbol literal
+puts nil #nil literal
+
+a = 10
+puts "My favorite number is #{a}"
+
+:name #symbol references something like a string
+
+puts 1, 2, 3, 50, 10, 4345098098 # Example of integer literals
+
+puts 1.2345, 2345.4267, 98.2234 # Example of float literals
+
+#nil within if will be treated as false
