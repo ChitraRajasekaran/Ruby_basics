a424df86d60e21930fe5ed9d0d35dfae6923354c
Chitra Rajasekaran <chitrarajasekaran@chitras-air.lan>
2022-11-20 22:42:26 -0500

    Day 1 The basics - part 2

--- Ruby_basics.rb
+++ Ruby_basics.rb
@@ -17,3 +17,9 @@ puts 1, 2, 3, 50, 10, 4345098098 # Example of integer literals
 puts 1.2345, 2345.4267, 98.2234 # Example of float literals
 
 #nil within if will be treated as false
+
+puts 1+1, 1-1,1*1,1/1,16%4, 4==3,4==4, 'foo'+'bar' #operations
+puts '12'.to_i, 5.to_s #Type conversions
+puts i = [1,2,3,4,5] , a = [1,2,3][0] #Array and index
+puts ({:dog => 'barks', :cat => 'meows', :pig => 'oinks'}) #hashes
+puts ({:dog => 'barks', :cat => 'meows', :pig => 'oinks'}[:cat]) #print value of a key
